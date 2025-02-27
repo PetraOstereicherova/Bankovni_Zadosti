ce0e8f827e3733e075c57c9b873f48ad0dabf54a
Petra Östereicherová <87311109+PetraOstereicherova@users.noreply.github.com>
2025-02-27 10:47:27 +0100

    Add files via upload
    1. Zadání projektu
    
    Název: 
    Optimalizace doby zpracování žádostí o bankovní produkty
    
    Cíl:
    
    - Zmapovat aktuální dobu zpracování žádostí (od podání po finální rozhodnutí)
    - Identifikovat úzká místa a neefektivní kroky
    -  Navrhnout optimalizaci pro rychlejší schválení
    
    
    Důvod:
    
    - Zrychlení procesu zlepšuje zákaznickou spokojenost
    - Snížení administrativní zátěže pro zaměstnance
    - Snížení nákladů díky efektivnějším procesům

---
+++ SQL_Schvalovani_zadosti.sql
@@ -0,0 +1,49 @@
+﻿--Výpočet průměrné doby schválení žádosti
+
+SELECT Produkt, AVG(DATEDIFF(HOUR, Datum_Podani, Datum_Schvaleni)) AS Prumerna_Doba_Hodin
+FROM BankovniZadosti
+WHERE Stav = 'Schváleno'
+GROUP BY Produkt;
+
+-- Identifikace nevyřízených žádostí
+
+SELECT * FROM BankovniZadosti WHERE Stav = 'Čeká na schválení';
+
+
+-- Rozdělení žádostí podle oddělení
+
+SELECT Odpovedny_utvar, COUNT(*) Pocet_zadosti
+FROM BankovniZadosti
+GROUP BY Odpovedny_Utvar;
+
+
+
+-- Nejčastější důvody zamítnutí
+
+SELECT Duvod_Odmitnuti,COUNT(*) pocet_odmitnutych
+FROM BankovniZadosti
+WHERE Stav = 'Odmítnuto'
+GROUP BY Duvod_Odmitnuti
+ORDER BY pocet_odmitnutych DESC;
+
+-- Kolik zamítnutých žádostí má každý produkt?
+
+SELECT Produkt, COUNT(*) AS Pocet_Odmitnutych
+FROM BankovniZadosti
+WHERE Stav = 'Odmítnuto'
+GROUP BY Produkt;
+
+
+-- Které oddělení zamítá nejvíce žádostí
+
+SELECT Odpovedny_Utvar, COUNT(*) AS pocet_odmitnutych
+FROM BankovniZadosti
+WHERE Stav = 'Odmítnuto'
+GROUP BY Odpovedny_Utvar;
+
+-- Zamítnuto s hodnoty NULL
+
+SELECT Stav, Duvod_Odmitnuti, COUNT(*)
+FROM BankovniZadosti
+WHERE Stav = 'Odmítnuto' AND Duvod_Odmitnuti IS NULL
+GROUP BY Stav, Duvod_Odmitnuti;
